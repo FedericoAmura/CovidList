c9ddbe4806d864add4813e3e41482cf5fefe8ecf
Federico Amura <federicoamura@gmail.com>
2022-02-28 21:30:03 -0300

    test: remove redux-flipper debugger middleware when running under jest

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -6,7 +6,7 @@ import rootReducer from './reducers';
 
 const middlewares = [thunkMiddleware];
 
-if (__DEV__) {
+if (__DEV__ && !process.env.JEST_WORKER_ID) {
   const createDebugger = require('redux-flipper').default;
   middlewares.push(createDebugger());
 }
